a658e57b9d7151d54921cee76350587a32fd30cc
Enzo Cervi <cervienzo7@gmail.com>
2024-09-05 12:25:35 -0300

    ABM TipoTramite - Falta condicion Doc

---
+++ admin-starter/src/main/java/ABMTipoTramite/dtos/TipoTramiteDTO.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ABMTipoTramite.dtos;
+
+import ABMTipoTramite.dtos.*;
+import java.sql.Timestamp;
+
+/**
+ *
+ * @author licciardi
+ */
+public class TipoTramiteDTO {
+    
+    private int codTipoTramite;
+    private String descripcionTipoTramite;
+    private String descripcionWebTipoTramite;
+    private Timestamp fechaHoraBajaTipoTramite;
+    private String nombreTipoTramite;
+    private int plazoEntregaDocumentacionTT;
+
+    public int getCodTipoTramite() {
+        return codTipoTramite;
+    }
+
+    public void setCodTipoTramite(int codTipoTramite) {
+        this.codTipoTramite = codTipoTramite;
+    }
+
+    public String getDescripcionTipoTramite() {
+        return descripcionTipoTramite;
+    }
+
+    public void setDescripcionTipoTramite(String descripcionTipoTramite) {
+        this.descripcionTipoTramite = descripcionTipoTramite;
+    }
+
+    public String getDescripcionWebTipoTramite() {
+        return descripcionWebTipoTramite;
+    }
+
+    public void setDescripcionWebTipoTramite(String descripcionWebTipoTramite) {
+        this.descripcionWebTipoTramite = descripcionWebTipoTramite;
+    }
+
+    public Timestamp getFechaHoraBajaTipoTramite() {
+        return fechaHoraBajaTipoTramite;
+    }
+
+    public void setFechaHoraBajaTipoTramite(Timestamp fechaHoraBajaTipoTramite) {
+        this.fechaHoraBajaTipoTramite = fechaHoraBajaTipoTramite;
+    }
+
+    public String getNombreTipoTramite() {
+        return nombreTipoTramite;
+    }
+
+    public void setNombreTipoTramite(String nombreTipoTramite) {
+        this.nombreTipoTramite = nombreTipoTramite;
+    }
+
+    public int getPlazoEntregaDocumentacionTT() {
+        return plazoEntregaDocumentacionTT;
+    }
+
+    public void setPlazoEntregaDocumentacionTT(int plazoEntregaDocumentacionTT) {
+        this.plazoEntregaDocumentacionTT = plazoEntregaDocumentacionTT;
+    }
+    
+    
+    
+}
