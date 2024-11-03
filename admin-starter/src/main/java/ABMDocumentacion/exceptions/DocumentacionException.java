c87afe6c5711246322b870fcad3c6ea38659507c
Fabrizio Azeglio <71147965+Fabros96@users.noreply.github.com>
2024-11-03 17:16:22 -0300

    Revert "ABM Documentacion Funcionando"
    This reverts commit c84d6add0ec9c48244aa7febe261adf814ffefa6.

--- admin-starter/src/main/java/ABMDocumentacion/exceptions/DocumentacionException.java
+++
@@ -1,7 +0,0 @@
-package ABMDocumentacion.exceptions;
-
-public class DocumentacionException extends Exception {
-    public DocumentacionException(String mensaje) {
-        super(mensaje);
-    }
-}
